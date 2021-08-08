21c5ca088dbc73d40439e2be20c9e6248851ddcd
lyhoanam-personal <lyhoanam@gmail.com>
2021-08-08 17:29:55 +0700

    add new menu

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
@@ -12,25 +12,27 @@ module.exports = {
     '@nuxtjs',
     'eslint:recommended',
     'plugin:nuxt/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
-  plugins: [
-  ],
+  plugins: [],
   // add your custom rules here
   rules: {
     semi: ['warn', 'always'],
     quotes: ['error', 'single'],
     'no-useless-catch': 'off',
     'vue/no-v-html': 2,
-    'no-irregular-whitespace': ['error', { 'skipComments': true }],
-    'vue/html-self-closing': ['error', {
-      'html': {
-        'void': 'always',
-        'normal': 'always',
-        'component': 'always'
+    'no-irregular-whitespace': ['error', { skipComments: true }],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
       },
-      'svg': 'always',
-      'math': 'always'
-    }]
-  }
-}
+    ],
+  },
+};
--- components/Header.vue
+++ components/Header.vue
@@ -3,15 +3,36 @@
     <header class="mn-container flex-center">
       <div class="container flex-between">
         <div class="align-center">
-          <eleIcon
-            :img-url="require('~/assets/images/Logo.png')"
-            img-class="ic-logo"
-          />
+          <nuxt-link to="/">
+            <eleIcon
+              :img-url="require('~/assets/images/Logo.png')"
+              img-class="ic-logo"
+            />
+          </nuxt-link>
           <eleNav />
         </div>
-        <menuUnlogin />
+        <menuChecked v-if="isMember" />
+        <menuUnlogin v-else />
       </div>
     </header>
     <div class="mn-mock" />
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      isMember: this.$route.name !== 'index',
+    };
+  },
+  watch: {
+    '$route.name': {
+      handler(name) {
+        this.isMember = name !== 'index';
+      },
+      deep: true,
+      immediate: true,
+    },
+  },
+};
+</script>
