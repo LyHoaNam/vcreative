28a2a1a78439372bab015a9da84e0f88ffb32978
lyhoanam-personal <lyhoanam@gmail.com>
2021-08-12 18:56:48 +0700

    add comment & relative tags

---
+++ components/ele/UserLo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex">
+    <div class="avatar-cmt" :style="{ backgroundImage: `url(${avatar})` }" />
+    <div class="pl-12 pt-4">
+      <div class="flex">
+        <p class="text-16-8-m">{{ name }}</p>
+        <p class="text-14-5 pl-14">{{ location }}</p>
+      </div>
+      <p class="text-14-5 pt-4">{{ date }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    avatar: {
+      type: String,
+      require: true,
+      default: '',
+    },
+    name: {
+      type: String,
+      require: true,
+      default: '',
+    },
+    date: {
+      type: String,
+      require: true,
+      default: '',
+    },
+    location: {
+      type: String,
+      require: true,
+      default: '',
+    },
+  },
+};
+</script>
--- pages/services/_slug.vue
+++ pages/services/_slug.vue
@@ -3,13 +3,15 @@
     <div class="container">
       <eleSubNav />
     </div>
-    <div class="container-sm row-des">
+    <div class="container-sm row-des p-s-d-feedback">
       <div class="col-left">
         <eleBreadCrumb class="pb-18" />
         <serviceDetailBillboard />
         <serviceDetailDescription />
         <serviceDetailSupplier />
         <serviceDetailPackage />
+        <serviceDetailFeedback />
+        <serviceDetailRelativeTags />
       </div>
       <div class="col-right">
         <serviceDetailInforBlock />
