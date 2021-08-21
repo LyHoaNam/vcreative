af2073b5b7eff33c597cc001928c79376310b5e9
lyhoanam-personal <lyhoanam@gmail.com>
2021-08-21 21:56:54 +0700

    add collaborators-register page

---
+++ layouts/collaborators.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <header class="mn-container">
+      <div class="container align-center">
+        <nuxt-link to="/">
+          <eleIcon
+            :img-url="require('~/assets/images/Logo.png')"
+            img-class="ic-logo"
+          />
+        </nuxt-link>
+        <ul class="ml-44 mn-step flex">
+          <li
+            v-for="(step, idx) in lsStep"
+            :key="`${idx}-step-collaborators`"
+            class="mn-step-item align-center"
+          >
+            <div class="mn-step-icon">{{ step.key }}</div>
+            <p class="ml-12 text-16-5">{{ step.txt }}</p>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <div class="mn-mock" />
+    <Nuxt />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      lsStep: [
+        {
+          key: '1',
+          txt: 'Thông tin cá nhân',
+        },
+        {
+          key: '2',
+          txt: 'Kỹ năng và kinh nghiệm',
+        },
+        {
+          key: '3',
+          txt: 'Liên kết và bảo mật tài khoản',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ pages/collaborators-register/index.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container-sm pt-40 pb-106">
+    <h1 class="text-32-8">Để tạo hồ sơ thành công</h1>
+    <p class="text-16-6">
+      Lưu ý tạo ấn tượng cho hồ sơ của bạn và những điều cần tránh
+    </p>
+    <div class="row co-intro">
+      <div class="col col-lg-6 co-intro-col-left">
+        <div class="align-center ml-50">
+          <div class="ic-co-intro-should-be" />
+          <p class="text-32-8-b ml-20">Tạo ấn tượng</p>
+        </div>
+        <div class="row mt-56">
+          <div
+            v-for="(item, idx) in lsShouldBe"
+            :key="`${idx}-should-be`"
+            class="col col-lg-6 co-intro-col-item"
+          >
+            <eleIcon :img-url="item.img" img-class="co-intro-ic" />
+            <p class="text-16-8 co-intro-des mt-8">{{ item.txt }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="col col-lg-6 co-intro-col-right">
+        <div class="align-center co-intro-title-not">
+          <div class="ic-co-intro-did-not" />
+          <p class="text-32-8-b ml-20">Cần tránh</p>
+        </div>
+        <div class="row mt-56">
+          <div
+            v-for="(item, idx) in lsDidnt"
+            :key="`${idx}-did-not`"
+            class="col col-lg-6 co-intro-col-item co-intro-item-r"
+          >
+            <div class="co-intro-des">
+              <eleIcon :img-url="item.img" img-class="co-intro-ic" />
+              <p class="text-16-8 mt-8">{{ item.txt }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <nuxt-link
+      to="/collaborators-register/profile"
+      class="btn-primary co-intro-btn-continue"
+    >
+      Tiếp tục
+    </nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'collaborators',
+  data() {
+    return {
+      lsShouldBe: [
+        {
+          txt: 'Dành thời gian của bạn trong việc tao hồ sơ của bạn để nó chính xác',
+          img: require('~/assets/images/icon/s-step1.png'),
+        },
+        {
+          txt: 'Mô tả chính xác các kỹ năng chuyên môn của bạn để giúp bạn nhận được nhiều việc hơn',
+          img: require('~/assets/images/icon/s-step2.png'),
+        },
+        {
+          txt: 'Tải lên ảnh hồ sơ cho thấy rõ khuôn mặt của bạn',
+          img: require('~/assets/images/icon/s-step3.png'),
+        },
+        {
+          txt: 'Thêm uy tín bằng cách liên kết các mạng chuyên nghiệp có liên quan của bạn',
+          img: require('~/assets/images/icon/s-step1.png'),
+        },
+      ],
+      lsDidnt: [
+        {
+          txt: 'Cung cấp bất kỳ thông tin sai lệch hoặc không chính xác về danh tính của bạn',
+          img: require('~/assets/images/icon/u-step1.png'),
+        },
+        {
+          txt: 'Hãy đảm bảo chắc chắn rằng những dự án bạn cung cấp là của chính bạn, không copy, sao chép ',
+          img: require('~/assets/images/icon/u-step2.png'),
+        },
+        {
+          txt: 'Không thực hiện liên lạc và thanh toán bên ngoài VCreative',
+          img: require('~/assets/images/icon/u-step3.png'),
+        },
+        {
+          txt: 'Không mở tài khoản trùng lặp. Bạn hoàn toàn có thể thêm nhiều dịch vụ để cung cấp. ',
+          img: require('~/assets/images/icon/u-step1.png'),
+        },
+      ],
+    };
+  },
+};
+</script>
