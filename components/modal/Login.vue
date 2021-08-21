69dddecfed9eb9396218b67264ec24d4ac366f1b
lyhoanam-personal <lyhoanam@gmail.com>
2021-08-21 13:20:02 +0700

    add logic validate login

--- components/modal/Login.vue
+++ components/modal/Login.vue
@@ -2,32 +2,51 @@
   <client-only>
     <modal name="Login" classes="modal-login" width="400px" height="auto">
       <p class="text-24-8-m text-center mb-32">Đăng nhập</p>
-      <button class="btn-fb mb-8">Tiếp tục bằng Facbook</button>
-      <button class="btn-google">Tiếp tục bằng Google</button>
+      <button class="btn-fb mb-8" @click="handleFb">
+        Tiếp tục bằng Facbook
+      </button>
+      <button class="btn-google" @click="handleGG">Tiếp tục bằng Google</button>
       <div class="ct-break-con pt-40 pb-42">
         <p class="ct-break">HOẶC</p>
       </div>
-      <form class="form-login">
+      <form class="form-login" @submit.prevent="handleSubmit">
         <input
+          v-model="valueEmail"
           class="input-normal mb-8"
           type="email"
           size="30"
           placeholder="Email / Tên đăng nhập"
+          @focus="isFocusEmail = true"
+          @blur="isFocusEmail = false"
         />
-        <div class="rel mb-16">
+        <p v-if="!isFocusEmail" class="pb-8 text-14-w1 mt-ne-4">
+          {{ messEmail }}
+        </p>
+        <div class="rel mb-4">
           <input
+            v-model="valuePass"
             :type="isShowPass ? 'text' : 'password'"
             size="30"
             class="input-pass"
             placeholder="Mật khẩu"
             autocomplete="current-password"
+            @focus="isFocusPas = true"
+            @blur="isFocusPas = false"
           />
           <label
             :class="`input-pass-ic ic-eye${isShowPass ? '' : '-off'}`"
             @click="isShowPass = !isShowPass"
           />
         </div>
-        <button class="btn-primary mb-12" disabled>Đăng nhập</button>
+        <p v-if="!isFocusPas" class="pb-16 text-14-w1">
+          {{ messPass }}
+        </p>
+        <button
+          class="btn-primary mb-12"
+          :disabled="!isMailCorrect || !isPassCorrect"
+        >
+          Đăng nhập
+        </button>
         <div class="align-between">
           <label class="ip-box-con">
             Ghi nhớ mật khẩu
@@ -44,12 +63,50 @@
   </client-only>
 </template>
 <script>
+import { validateEmail } from '~/utils/common.js';
 export default {
   name: 'Login',
   data() {
     return {
       isShowPass: false,
+      valueEmail: '',
+      valuePass: '',
+      messEmail: '',
+      messPass: '',
+      isMailCorrect: false,
+      isPassCorrect: false,
+      isFocusEmail: true,
+      isFocusPas: true,
     };
+  },
+  watch: {
+    valueEmail: {
+      handler(value) {
+        if (!value) {
+          this.messEmail = 'Bạn chưa nhập email';
+          this.isMailCorrect = false;
+        } else if (!validateEmail(value)) {
+          this.messEmail = 'Email không đúng, vui lòng thử lại';
+          this.isMailCorrect = false;
+        } else {
+          this.messEmail = '';
+          this.isMailCorrect = true;
+        }
+      },
+      deep: true,
+    },
+    valuePass: {
+      handler(value) {
+        if (!value) {
+          this.messPass = 'Bạn chưa nhập mật khẩu';
+          this.isPassCorrect = false;
+        } else {
+          this.messPass = '';
+          this.isPassCorrect = true;
+        }
+      },
+      deep: true,
+    },
   },
   methods: {
     show() {
@@ -57,6 +114,19 @@ export default {
     },
     hide() {
       this.$modal.hide('my-first-modal');
+    },
+    handleFb() {
+      this.$auth.loginWith('facebook', { params: {} }).then((response) => {
+        console.log(response);
+      });
+    },
+    handleGG() {
+      this.$auth.loginWith('google', { params: {} }).then((response) => {
+        console.log(response);
+      });
+    },
+    handleSubmit() {
+      console.log('submit');
     },
   },
   mount() {
--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,11 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        name: 'google-signin-client_id',
+        content:
+          '1051508446752-0f5gg4vgc4tni60n96m5c6vp39cp7lmd.apps.googleusercontent.com',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
@@ -20,6 +25,44 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [{ src: '~/plugins/swiper.js' }, { src: '~/plugins/modal.js' }],
 
+  auth: {
+    strategies: {
+      local: {
+        token: {
+          property: 'token',
+          global: true,
+          // required: true,
+          // type: 'Bearer'
+        },
+        user: {
+          property: 'user',
+          // autoFetch: true
+        },
+        endpoints: {
+          login: { url: '/api/auth/login', method: 'post' },
+          logout: { url: '/api/auth/logout', method: 'post' },
+          user: { url: '/api/auth/user', method: 'get' },
+        },
+      },
+      facebook: {
+        endpoints: {
+          userInfo:
+            'https://graph.facebook.com/v6.0/me?fields=id,name,picture{url}',
+        },
+        clientId: '558339925417449',
+        scope: ['public_profile', 'email'],
+      },
+      google: {
+        clientId:
+          '1051508446752-0f5gg4vgc4tni60n96m5c6vp39cp7lmd.apps.googleusercontent.com',
+        audience: 'http://localhost:3000',
+        scope: ['openid', 'profile', 'email', 'offline_access'],
+        responseType: 'code',
+        grantType: 'authorization_code',
+        codeChallengeMethod: 'S256',
+      },
+    },
+  },
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
@@ -30,9 +73,11 @@ export default {
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
   ],
-
+  router: {
+    // middleware: ['auth'],
+  },
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxtjs/axios', '@nuxtjs/auth-next'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
