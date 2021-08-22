8fc1454b32f84d5618a3d9bb692f06199131e11c
lyhoanam-personal <lyhoanam@gmail.com>
2021-08-22 19:45:55 +0700

    complete profile collaborators

--- pages/collaborators-register/profile/index.vue
+++ pages/collaborators-register/profile/index.vue
@@ -20,10 +20,32 @@
           </p>
         </div>
         <div class="co-pro-col-right">
-          <eleIcon
-            img-url="https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-            img-class="card-pro-avatar"
-          />
+          <client-only>
+            <image-uploader
+              :preview="true"
+              :class-name="['fileinput']"
+              capture="environment"
+              :debug="1"
+              do-not-resize="gif"
+              :auto-rotate="true"
+              output-format="verbose"
+              class="co-pro-avatar"
+              @input="setImage"
+            >
+              <label
+                slot="upload-label"
+                for="fileInput"
+                :class="`co-pro-avatar-thumb flex-center${
+                  hasImg ? ' op-hide' : ''
+                }`"
+              >
+                <figure class="text-center">
+                  <p class="text-32-6">+</p>
+                  <p class="text-12-6">thêm ảnh</p>
+                </figure>
+              </label>
+            </image-uploader>
+          </client-only>
         </div>
       </div>
       <div class="align-center mt-40">
@@ -75,13 +97,19 @@
           Khu vực<span class="text-16-w1"> *</span>
         </div>
         <div class="co-pro-col-right">
-          <input
-            class="input-normal"
-            placeholder=""
-            type="number"
-            pattern="[0-9]*"
-            inputmode="numeric"
-          />
+          <client-only>
+            <VSelect
+              v-model="provicenSelected"
+              :options="province"
+              label="name"
+              placeholder="Chọn tỉnh thành"
+            >
+              <!-- eslint-disable-next-line vue/no-unused-vars  -->
+              <template #no-options="{ search, searching, loading }">
+                Có vẻ như không có tỉnh thành nào đúng với ký tự tìm của bạn
+              </template>
+            </VSelect>
+          </client-only>
         </div>
       </div>
       <div class="align-center mt-24">
@@ -106,12 +134,22 @@
   </div>
 </template>
 <script>
+import { province } from '~/utils/provinces.js';
 export default {
   layout: 'collaborators',
   data() {
     return {
       valueDes: '',
+      provicenSelected: '',
+      hasImg: false,
+      province,
     };
+  },
+  methods: {
+    setImage(output) {
+      this.image = output;
+      this.hasImg = true;
+    },
   },
 };
 </script>
