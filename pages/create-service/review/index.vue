a5695316953c363718a3075ae4d6433cef05aa43
lyhoanam-personal <lyhoanam@gmail.com>
2021-08-31 13:55:08 +0700

    add page review

---
+++ pages/create-service/review/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container-xsm pt-40 pb-106">
+    <div class="cre-se-container">
+      <h1 class="text-32-8 mb-12">Xem lại dưới góc nhìn của khách hàng</h1>
+      <h3 class="text-16-6 font-regular co-pro-des">
+        Xem lại các thông tin dịch vụ của bạn trước khi quảng cáo đến khách hàng
+        của bạn
+      </h3>
+      <div class="co-pro-hr mt-56 cre-se-container" />
+      <eleBreadCrumb />
+      <serviceDetailBillboard />
+      <serviceDetailDescription />
+      <serviceDetailSupplier />
+      <serviceDetailPackage />
+      <div class="flex-between mt-56">
+        <div />
+        <div class="flex">
+          <nuxt-link
+            to="/create-service/pictures"
+            class="btn-line-5 mr-24 cre-se-cancel"
+            >Quay lại</nuxt-link
+          >
+          <nuxt-link to="#" class="btn-primary cre-se-submit">
+            Lưu &#38; Tiếp tục
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'service',
+};
+</script>
